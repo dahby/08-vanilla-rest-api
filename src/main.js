6b1e92728f05431aeff97db3d7cc7c04ac348c43
dahby <dstoll243@gmail.com>
2018-04-25 13:25:41 -0700

    main & logger js files completed

---
+++ src/main.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const server = require('./lib/server');
+const logger = require('./lib/logger');
+
+const PORT = process.env.PORT || 3000;
+
+server.start(PORT, () => logger.log(logger.INFO, `MAIN: listening on ${PORT}`));
