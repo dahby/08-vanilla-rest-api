e7dd22628cae53739c859454924243fa21075af9
dahby <dstoll243@gmail.com>
2018-04-25 17:22:01 -0700

    at a stopping point

--- src/__test__/server.test.js
+++ src/__test__/server.test.js
@@ -5,3 +5,22 @@ const superagent = require('superagent');
 
 const testPort = 5000;
 const mockResource = { title: 'test title', content: 'test content' };
+let mockId = null;
+
+beforeAll(() => server.start(testPort));
+afterAll(() => server.stop());
+
+describe('VALID request to the API', () => {
+  describe('POST /api/v1/food', () => {
+    test('should respond with a status 201 and create a new food', () => {
+      return superagent.post(`:${testPort}/api/v1/food`)
+        .send(mockResource)
+        .then((res) => {
+          mockId = res.body.id;
+          expect(res.body.title).toEqual(mockResource.title);
+          expect(res.body.content).toEqual(mockResource.content);
+          expect(res.status).toEqual(201);
+        });
+    });
+  });
+});
