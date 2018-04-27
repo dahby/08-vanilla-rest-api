2e234db35c9d36f19e19325404320c8890a9e8b8
dahby <dstoll243@gmail.com>
2018-04-27 05:58:53 -0700

    all GET tests passing

--- src/__test__/server.test.js
+++ src/__test__/server.test.js
@@ -40,20 +40,30 @@ describe('VALID request to the API', () => {
           expect(res.status).toEqual(200);
         });
     });
-  //   test('No id should result in 404', () => {
-  //     return superagent.get(`:${testPort}/api/v1/food?id=123`)
-  //       .then((res) => {
-  //         expect(res.status).toEqual(404);
-  //       });
-  //   });
-  });
-  describe('GET /api/v1/food', () => {
-    test('should retrieve all food', () => {
-      return superagent.get(`:${testPort}/api/v1/food`)
-        .then((res) => {
-          expect(res.body).toEqual([mockId]);
-          expect(res.status).toEqual(200);
+    test('Bad id should result in 400', () => {
+      return superagent.get((`:${testPort}/api/v1/food`))
+        .then(() => {})
+        .catch((err) => {
+          expect(err).toBeTruthy();
+          expect(err.status).toEqual(400);
         });
+    });
+    test('No id should result in 404', () => {
+      return superagent.get(`:${testPort}/api/v1/ford`)
+        .then(() => {})
+        .catch((err) => {
+          expect(err).toBeTruthy();
+          expect(err.status).toEqual(404);
+        }); 
     });
   });
 });
+//   describe('GET /api/v1/food', () => {
+//     test('should retrieve all food', () => {
+//       return superagent.get(`:${testPort}/api/v1/food`)
+//         .then((res) => {
+//           expect(res.body).toEqual([mockId]);
+//           expect(res.status).toEqual(200);
+//         });
+//     });
+//   });
